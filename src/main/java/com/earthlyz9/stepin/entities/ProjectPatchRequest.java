1d4a28d8664539dfe64ba5724bae7ef82f0bd50e
linda2927 <linda2927@naver.com>
2023-04-04 16:19:46 +0900

    feat: add additional class for patch updates

---
+++ src/main/java/com/earthlyz9/stepin/entities/ProjectPatchRequest.java
@@ -0,0 +1,16 @@
+package com.earthlyz9.stepin.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectPatchRequest {
+    private String name;
+}
