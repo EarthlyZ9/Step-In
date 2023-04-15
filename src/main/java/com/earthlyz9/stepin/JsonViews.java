64f1862b431c32cc0362499f924d8697c444df36
linda2927 <linda2927@naver.com>
2023-04-15 18:01:49 +0900

    feat: add JsonViews class for Jackson Filtering

---
+++ src/main/java/com/earthlyz9/stepin/JsonViews.java
@@ -0,0 +1,7 @@
+package com.earthlyz9.stepin;
+
+public class JsonViews {
+    public static class Base {};
+    public static class List extends Base {};
+    public static class Retrieve extends Base {};
+}
