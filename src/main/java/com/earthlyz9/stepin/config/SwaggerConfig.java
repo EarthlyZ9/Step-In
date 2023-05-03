9334341402ef15e8e47b30252aca88481dcda681
linda2927 <linda2927@naver.com>
2023-05-04 08:23:32 +0900

    feat: add security schema to swagger config

--- src/main/java/com/earthlyz9/stepin/config/SwaggerConfig.java
+++ src/main/java/com/earthlyz9/stepin/config/SwaggerConfig.java
@@ -1,31 +1,32 @@
 package com.earthlyz9.stepin.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import jakarta.persistence.Entity;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.stereotype.Service;
 
 @OpenAPIDefinition(
     info = @Info(
-        title = "spring commerce API 명세서",
-        description = "API 명세서",
+        title = "Step In Project",
+        description = "A new sketchbook for your API",
         version = "v1",
         contact = @Contact(
-            name = "beginners",
-            email = "sample@email.co.kr"
+            name = "EarthlyZ9",
+            email = "earthlyz9.dev@gmail.com"
         )
     )
 )
-// TODO: SecuritySchemes
+@SecuritySchemes({
+    @SecurityScheme(name = "Bearer Token",
+        type = SecuritySchemeType.APIKEY,
+        description = "Jwt bearer token",
+        in = SecuritySchemeIn.HEADER,
+        paramName = "Authorization"),
+})
 @Configuration
 public class SwaggerConfig {
 //
