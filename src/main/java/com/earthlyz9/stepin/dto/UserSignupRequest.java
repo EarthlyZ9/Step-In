b707bdeb634822efd317af9acd6dfced6fddf02d
linda2927 <linda2927@naver.com>
2023-05-04 08:24:00 +0900

    feat: add schema information for login and signup dto

--- src/main/java/com/earthlyz9/stepin/dto/UserSignupRequest.java
+++ src/main/java/com/earthlyz9/stepin/dto/UserSignupRequest.java
@@ -2,6 +2,10 @@ package com.earthlyz9.stepin.dto;
 
 import com.earthlyz9.stepin.entities.User;
 import com.earthlyz9.stepin.entities.UserRole;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,11 +20,25 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserSignupRequest {
     @Size(max = 100)
+    @NotBlank
+    @Schema(description = "이메일", example = "example@email.com")
+    @Email
     private String email;
 
     @Size(max = 50)
+    @NotBlank
+    @NotNull
+    @Schema(description = "사용할 이름", example = "지구")
     private String nickname;
+
+    @NotBlank
+    @NotNull
+    @Schema(description = "비밀번호", example = "password")
     private String password;
+
+    @NotBlank
+    @NotNull
+    @Schema(description = "비밀번호 재입력", example = "password")
     private String confirmPassword;
 
     public User toEntity(String encodedPassword) {
