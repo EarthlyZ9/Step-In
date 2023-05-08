f8c8726a9d65ff6b32f11a8960e7b790dc473211
linda2927 <linda2927@naver.com>
2023-05-08 10:19:31 +0900

    feat: add ProjectResourceAssembler for HATEOAS

---
+++ src/main/java/com/earthlyz9/stepin/assemblers/ProjectResourceAssembler.java
@@ -0,0 +1,33 @@
+package com.earthlyz9.stepin.assemblers;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import com.earthlyz9.stepin.controllers.ProjectController;
+import com.earthlyz9.stepin.dto.ProjectDto;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProjectResourceAssembler implements RepresentationModelAssembler<ProjectDto, EntityModel<ProjectDto>> {
+    @Override
+    public CollectionModel<EntityModel<ProjectDto>> toCollectionModel(Iterable<? extends ProjectDto> projects) {
+        return RepresentationModelAssembler.super.toCollectionModel(projects).add(
+            linkTo(methodOn(ProjectController.class).getAllProjects()).withSelfRel()
+        );
+    }
+
+    @Override
+    public EntityModel<ProjectDto> toModel(ProjectDto project) {
+        int projectId = project.getId();
+
+        EntityModel<ProjectDto> entityModel = EntityModel.of(project,
+            linkTo(methodOn(ProjectController.class).getProjectById(projectId)).withSelfRel());
+        entityModel.add(
+            linkTo(methodOn(ProjectController.class).getAllProjects()).withRel("collection"));
+
+        return entityModel;
+    }
+}
