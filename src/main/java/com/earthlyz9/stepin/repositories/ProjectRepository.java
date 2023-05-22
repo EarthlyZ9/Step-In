9f74b5e2946916d76cd0c91b210516878c30c73f
linda2927 <linda2927@naver.com>
2023-05-22 16:47:53 +0900

    fix: get project list ordered by updatedAt descending order

--- src/main/java/com/earthlyz9/stepin/repositories/ProjectRepository.java
+++ src/main/java/com/earthlyz9/stepin/repositories/ProjectRepository.java
@@ -5,5 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
+    List<Project> findByOwnerIdOrderByUpdatedAtDesc(Integer ownerId);
     List<Project> findByOwnerId(Integer ownerId);
 }
